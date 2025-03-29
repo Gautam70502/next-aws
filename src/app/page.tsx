4a31a3f13220b8906c89ac7826e96e6b37bd49ac
Gautam70502 <gautamrathod70502@gmail.com>
2025-03-29 22:14:50 +0530

    fix: yml changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>Learn about Next.js Next.js Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
